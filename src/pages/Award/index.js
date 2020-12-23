bd34f01700f561f8c92151ba2b209c0f3ac646b7
mjsil <m.j.silveira@outlook.com>
2020-12-23 11:39:46 -0300

    v5.5

--- src/pages/Award/index.js
+++ src/pages/Award/index.js
@@ -1,55 +1,103 @@
 import React, { useState, useEffect } from 'react';
 import { ScrollView, Platform } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import api from '../../services';
 
 import Container from '../../components/Container';
 
 import {
-        ContentFull, Header, HeaderLabel, Contracts, Contract, ContentName, Card, CardLabel, Name, Content, ContentValue, Label, Value
+    ContentFull,
+    Header,
+    HeaderLabel,
+    Contracts,
+    Contract,
+    ContentName,
+    Card,
+    CardLabel,
+    Name,
+    Content,
+    ContentValue,
+    Label,
+    Value,
 } from './styles';
 
 const allNews = [
     {
-        contrato: "14523214547854125412",
-        date: "12/09/2020",
-        valor: "R$ 500,00",
-        pago: false
+        contrato: '14523214547854125412',
+        date: '12/09/2020',
+        valor: 'R$ 500,00',
+        pago: false,
     },
     {
-        contrato: "14523214547854125412",
-        date: "12/09/2020",
-        valor: "R$ 500,00",
-        pago: false
+        contrato: '14523214547854125412',
+        date: '12/09/2020',
+        valor: 'R$ 500,00',
+        pago: false,
     },
     {
-        contrato: "14523214547854125412",
-        date: "12/09/2020",
-        valor: "R$ 500,00",
-        pago: true
+        contrato: '14523214547854125412',
+        date: '12/09/2020',
+        valor: 'R$ 500,00',
+        pago: true,
     },
     {
-        contrato: "14523214547854125412",
-        date: "12/09/2020",
-        valor: "R$ 500,00",
-        pago: true
+        contrato: '14523214547854125412',
+        date: '12/09/2020',
+        valor: 'R$ 500,00',
+        pago: true,
     },
     {
-        contrato: "14523214547854125412",
-        date: "12/09/2020",
-        valor: "R$ 500,00",
-        pago: true
+        contrato: '14523214547854125412',
+        date: '12/09/2020',
+        valor: 'R$ 500,00',
+        pago: true,
     },
     {
-        contrato: "14523214547854125412",
-        date: "12/09/2020",
-        valor: "R$ 500,00",
-        pago: true
+        contrato: '14523214547854125412',
+        date: '12/09/2020',
+        valor: 'R$ 500,00',
+        pago: true,
     },
-]
+];
 
 const Award = () => {
     const isIos = Platform.OS === 'ios' ? true : false;
-    const navigation = useNavigation();
+
+    const [contracts, setContracts] = useState([]);
+    const [token, setToken] = useState('');
+
+    useEffect(() => {
+        const getUserName = async () => {
+            const getToken = await AsyncStorage.getItem('loginToken');
+
+            if (getToken) {
+                setToken(getToken);
+            }
+        };
+
+        getUserName();
+    }, []);
+
+    useEffect(() => {
+        const getContracts = async () => {
+            try {
+                const response = await api.get('/all/contracts/active', {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+
+                setContracts(response.data.Contracts);
+            } catch (err) {
+                console.log('ERRO: ', err);
+
+                //COLOLAR MODAL QUE NÃO ENCONTROU OS CONTRATOS
+            }
+        };
+
+        getContracts();
+    }, [token]);
 
     return (
         <Container>
@@ -63,34 +111,40 @@ const Award = () => {
                             paddingTop: 16,
                         }}
                     >
-                        {
-                            allNews.map((news, index) => (
-                                <Contract key={index} onPress={() => {}}>
-                                    <ContentName>
-                                        <Label>contrato: </Label>
-                                        <Name>{news.contrato}</Name>
-                                    </ContentName>
-                                    <Content>
-                                        <ContentValue>
-                                            <Label>Valor</Label>
-                                            <Value>{news.valor}</Value>
-                                        </ContentValue>
-                                        <ContentValue>
-                                            <Label>Disponível</Label>
-                                            <Value>{news.date}</Value>
-                                        </ContentValue>
-                                    </Content>
-                                    <Card pago={news.pago}>
-                                        <CardLabel>{news.pago ? "Pagamento Efetuado" : "Aguarde Pagamento"}</CardLabel>
-                                    </Card>
-                                </Contract>
-                            ))
-                        }
+                        {allNews.map((news, index) => (
+                            <Contract key={index} onPress={() => {}}>
+                                <ContentName>
+                                    <Label>Cliente: </Label>
+                                    <Name>{news.contrato}</Name>
+                                </ContentName>
+                                <ContentName>
+                                    <Label>Contrato: </Label>
+                                    <Name>{news.contrato}</Name>
+                                </ContentName>
+                                <Content>
+                                    <ContentValue>
+                                        <Label>Valor</Label>
+                                        <Value>{news.valor}</Value>
+                                    </ContentValue>
+                                    <ContentValue>
+                                        <Label>Disponível</Label>
+                                        <Value>{news.date}</Value>
+                                    </ContentValue>
+                                </Content>
+                                <Card pago={news.pago}>
+                                    <CardLabel>
+                                        {news.pago
+                                            ? 'Pagamento Efetuado'
+                                            : 'Aguarde Pagamento'}
+                                    </CardLabel>
+                                </Card>
+                            </Contract>
+                        ))}
                     </Contracts>
                 </ScrollView>
             </ContentFull>
         </Container>
     );
-}
+};
 
 export default Award;
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Platform } from 'react-native';
 import { Ionicons, FontAwesome } from '@expo/vector-icons';
@@ -7,33 +7,80 @@ import { useNavigation } from '@react-navigation/native';
 import { parseISO } from 'date-fns';
 import { format } from 'date-fns-tz';
 import { ptBR } from 'date-fns/locale';
+import intl from 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
 
 import api from '../../services';
 
 import Container from '../../components/Container';
 
 import {
-        Content, ContentHeader, Header, Icon, HeaderLabel, Ball, Banner, BannerLabel, Graphic, MoreInfo, Label, ContentValue, Currency,
-        Value, GraphicCurrency, GraphicGoal, Cards, Card, CardTitle, Infos, Title, ContractTitle, Info, Name, Description, Date,
-        State, LabelState
+    Content,
+    ContentHeader,
+    Header,
+    Icon,
+    HeaderLabel,
+    Ball,
+    Banner,
+    BannerLabel,
+    Graphic,
+    MoreInfo,
+    Label,
+    ContentValue,
+    Currency,
+    Value,
+    GraphicCurrency,
+    GraphicGoal,
+    Cards,
+    Card,
+    CardTitle,
+    Infos,
+    Title,
+    ContractTitle,
+    Info,
+    Name,
+    Description,
+    Date,
+    State,
+    LabelState,
 } from './styles';
 
 const Dashboard = () => {
     const metaAtual = 13000;
-    const valorAtual = 3000;
 
+    const windowHeight = Dimensions.get('window').height;
     const isIos = Platform.OS === 'ios' ? true : false;
     const navigation = useNavigation();
+
     const [contracts, setContracts] = useState([]);
+    const [contractsCurrent, setContractsCurrent] = useState([]);
     const [prospections, setProspections] = useState([]);
     const [goalHeight, setGoalHeight] = useState(0); //META
-    const [currencyHeight, setCurrencyHeight] = useState(0); //ATUAL
     const [userName, setUserName] = useState('');
     const [token, setToken] = useState('');
-    const windowHeight = Dimensions.get('window').height;
 
     useEffect(() => {
-        const getContracts = async() => {
+        const getAllContractsCurrent = async () => {
+            try {
+                const response = await api.get('/contracts/date', {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+
+                setContractsCurrent(response.data.Contracts);
+            } catch (err) {
+                console.log('ERRO: ', err);
+
+                //COLOLAR MODAL QUE NÃO ENCONTROU OS CONTRATOS
+            }
+        };
+
+        getAllContractsCurrent();
+    }, [token]);
+
+    useEffect(() => {
+        const getContracts = async () => {
             try {
                 const response = await api.get('/contracts', {
                     headers: {
@@ -42,18 +89,18 @@ const Dashboard = () => {
                 });
 
                 setContracts(response.data.Contracts);
-            } catch(err) {
+            } catch (err) {
                 console.log('ERRO: ', err);
 
                 //COLOLAR MODAL QUE NÃO ENCONTROU OS CONTRATOS
             }
-        }
+        };
 
         getContracts();
     }, [token]);
 
     useEffect(() => {
-        const getProspections = async() => {
+        const getProspections = async () => {
             try {
                 const response = await api.get('/prospections', {
                     headers: {
@@ -62,97 +109,118 @@ const Dashboard = () => {
                 });
 
                 setProspections(response.data.Prospections);
-            } catch(err) {
+            } catch (err) {
                 console.log('ERRO: ', err);
 
                 //COLOLAR MODAL QUE NÃO ENCONTROU PROSPECÇÕES
             }
-        }
+        };
 
         getProspections();
     }, [token]);
 
     useEffect(() => {
-
-        const getUserName = async() => {
+        const getUserName = async () => {
             const getName = await AsyncStorage.getItem('userName');
             const getToken = await AsyncStorage.getItem('loginToken');
 
-            if(getName) {
+            if (getName) {
                 setUserName(getName);
             }
 
-            if(getToken) {
+            if (getToken) {
                 setToken(getToken);
             }
-        }
+        };
 
         getUserName();
-    },[]);
+    }, []);
 
-    useEffect(() => {
-        const getValueHeight = () => {
-            const currencyX = (goalHeight * valorAtual) / metaAtual;
-            const currency = ((currencyX * 100) / goalHeight).toFixed(2);
+    const totalContracts = useMemo(() => {
+        const total = contractsCurrent.reduce((accumulator, current, index) => {
+            return index + 1;
+        }, 0);
 
-            if(currency > 100) {
-                setCurrencyHeight(goalHeight);
-            }else {
-                setCurrencyHeight((currency * goalHeight) / 100);
-            }
+        return total;
+    }, [contractsCurrent]);
+
+    const totalMoney = useMemo(() => {
+        const total = contractsCurrent.reduce((accumulator, current) => {
+            return accumulator + current.VALORTOTAL;
+        }, 0);
+
+        return new Intl.NumberFormat('pt-BR').format(total);
+    }, [contractsCurrent]);
+
+    const currencyHeight = useMemo(() => {
+        const currencyX =
+            (goalHeight *
+                parseFloat(totalMoney.replace('.', '').replace(',', '.'))) /
+            metaAtual;
+        const currency = ((currencyX * 100) / goalHeight).toFixed(2);
+
+        if (currency > 100) {
+            return goalHeight;
         }
 
-        getValueHeight();
-    }, [goalHeight, currencyHeight]);
-
+        return (currency * goalHeight) / 100;
+    }, [contractsCurrent]);
 
     const goNews = () => {
         navigation.navigate('News');
-    }
+    };
 
     const goContracts = () => {
         navigation.navigate('Contracts');
-    }
+    };
 
     const goProspections = () => {
         navigation.navigate('Prospections');
-    }
+    };
 
     const goContractInfo = (contract) => {
         navigation.navigate('More', {
             info: {
                 ...contract,
                 contract: true,
-            }
+            },
         });
-    }
+    };
 
     const goProspectionInfo = (prospection) => {
         navigation.navigate('More', {
             info: {
                 ...prospection,
                 prospection: true,
-            }
+            },
         });
-    }
+    };
 
     return (
         <Container>
             <Content windowHeight={isIos}>
-                <ContentHeader windowHeight={isIos ? (windowHeight / 1.20) : windowHeight}>
+                <ContentHeader
+                    windowHeight={isIos ? windowHeight / 1.2 : windowHeight}
+                >
                     <Header>
                         <HeaderLabel>{userName}</HeaderLabel>
                         <Icon onPress={() => goNews()}>
                             <Ball />
-                            <Ionicons name='md-notifications' size={30} color='#fff' />
+                            <Ionicons
+                                name="md-notifications"
+                                size={30}
+                                color="#fff"
+                            />
                         </Icon>
                     </Header>
                 </ContentHeader>
-                <Banner onLayout={(event) => {
-                    const { height } = event.nativeEvent.layout;
+                <Banner
+                    onLayout={(event) => {
+                        const { height } = event.nativeEvent.layout;
 
-                    setGoalHeight((height - 32).toFixed(2));
-                }}>
+                        setGoalHeight((height - 32).toFixed(2));
+                    }}
+                >
                     <BannerLabel>
                         <MoreInfo>
                             <Label>Saldo Atual</Label>
@@ -160,7 +228,7 @@ const Dashboard = () => {
                                 <Currency>
                                     <Label currency={true}>R$</Label>
                                 </Currency>
-                                <Value>{valorAtual},00</Value>
+                                <Value>{totalMoney}</Value>
                             </ContentValue>
                         </MoreInfo>
                         <MoreInfo>
@@ -181,7 +249,7 @@ const Dashboard = () => {
                 <Cards>
                     <Card>
                         <CardTitle>Contratos Atuais</CardTitle>
-                        <Value>13</Value>
+                        <Value>{totalContracts}</Value>
                     </Card>
                     <Card>
                         <CardTitle>Meta Atual</CardTitle>
@@ -192,71 +260,82 @@ const Dashboard = () => {
                     <Header>
                         <Title>Contratos</Title>
                         <Icon onPress={() => goContracts()}>
-                            <FontAwesome name='eye' size={22} color='#41484A' />
+                            <FontAwesome name="eye" size={22} color="#41484A" />
                             <ContractTitle>Ver Todos</ContractTitle>
                         </Icon>
                     </Header>
-                    {
-                        contracts.map((contract, index) => (
-                            <Info key={index} onPress={() => goContractInfo(contract)}>
-                                <Description>
-                                    <Name>Código: {contract.CODCONTRATO}</Name>
-                                    <Date>Data:
-                                        {format(parseISO(contract.DATAINICIO),
-                                            " dd 'de' MMMM 'de' yyyy'",{
-                                                locale: ptBR
-                                            }
-                                        )}
-                                    </Date>
-                                </Description>
-                                <State cod={2}>
-                                    <LabelState>Ativo</LabelState>
-                                </State>
-                            </Info>
-                        ))
-                    }
+                    {contracts.map((contract, index) => (
+                        <Info
+                            key={index}
+                            onPress={() => goContractInfo(contract)}
+                        >
+                            <Description>
+                                <Name>Código: {contract.CODCONTRATO}</Name>
+                                <Date>
+                                    Data:
+                                    {format(
+                                        parseISO(contract.DATAINICIO),
+                                        " dd 'de' MMMM 'de' yyyy'",
+                                        {
+                                            locale: ptBR,
+                                        }
+                                    )}
+                                </Date>
+                            </Description>
+                            <State cod={2}>
+                                <LabelState>Ativo</LabelState>
+                            </State>
+                        </Info>
+                    ))}
                 </Infos>
                 <Infos>
                     <Header>
                         <Title>Prospecções</Title>
                         <Icon onPress={() => goProspections()}>
-                            <FontAwesome name='eye' size={22} color='#41484A' />
+                            <FontAwesome name="eye" size={22} color="#41484A" />
                             <ContractTitle>Ver Todas</ContractTitle>
                         </Icon>
                     </Header>
-                    {
-                        prospections.map((prospection, index) => (
-                            <Info key={index} onPress={() => goProspectionInfo(prospection)} prospection={true}>
-                                <Description>
-                                    <Name>Empresa: {prospection.EMPRESA}</Name>
-                                    <Date>Data:
-                                        {format(parseISO(prospection.DATACADASTRO),
-                                            " dd 'de' MMMM 'de' yyyy'",{
-                                                locale: ptBR
-                                            }
-                                        )}
-                                    </Date>
-                                </Description>
-                                <State prospection={true} style={{ width: '100%' }} cod={prospection.CODSTATUSPROSPECCAO}>
-                                    <LabelState>
+                    {prospections.map((prospection, index) => (
+                        <Info
+                            key={index}
+                            onPress={() => goProspectionInfo(prospection)}
+                            prospection={true}
+                        >
+                            <Description>
+                                <Name>Empresa: {prospection.EMPRESA}</Name>
+                                <Date>
+                                    Data:
+                                    {format(
+                                        parseISO(prospection.DATACADASTRO),
+                                        " dd 'de' MMMM 'de' yyyy'",
                                         {
-                                            (prospection.CODSTATUSPROSPECCAO === 1)
-                                            ?   'Em Digitação'
-                                            :   (prospection.CODSTATUSPROSPECCAO === 2)
-                                            ?   'Contrato Gerado'
-                                            :   (prospection.CODSTATUSPROSPECCAO === 3)
-                                            ?   'Cliente Desistiu'
-                                            :   'Sem Status'
+                                            locale: ptBR,
                                         }
-                                    </LabelState>
-                                </State>
-                            </Info>
-                        ))
-                    }
+                                    )}
+                                </Date>
+                            </Description>
+                            <State
+                                prospection={true}
+                                style={{ width: '100%' }}
+                                cod={prospection.CODSTATUSPROSPECCAO}
+                            >
+                                <LabelState>
+                                    {prospection.CODSTATUSPROSPECCAO === 1
+                                        ? 'Em Digitação'
+                                        : prospection.CODSTATUSPROSPECCAO === 2
+                                        ? 'Contrato Gerado'
+                                        : prospection.CODSTATUSPROSPECCAO === 3
+                                        ? 'Cliente Desistiu'
+                                        : 'Sem Status'}
+                                </LabelState>
+                            </State>
+                        </Info>
+                    ))}
                 </Infos>
             </Content>
         </Container>
     );
-}
+};
 
 export default Dashboard;
